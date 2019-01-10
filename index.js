d1aa415b34cd28340e1dfa07bea438b37f9c40f6
Riccardo <riccardo.lorenzon@gmail.com>
2019-01-10 21:30:38 +0100

    Refactor modules.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
-
-import Header from './src/components/header';
-import AlbumList from './src/components/AlbumList';
-
-const App = () => (
-    <View style={{ flex: 1 }}>
-      <Header headerText={'Albums'} />
-      <AlbumList />
-    </View>
-);
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
 AppRegistry.registerComponent('albums_app', () => App);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Header } from './components/common/Header';
+import AlbumList from './components/AlbumList';
+
+const App = () => (
+    <View style={{ flex: 1 }}>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+);
+
+export default App;
