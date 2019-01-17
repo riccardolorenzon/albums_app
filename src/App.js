517b05136cfd05670deb1a5cf691d2e8b6b70ebb
Riccardo <riccardo.lorenzon@gmail.com>
2019-01-17 22:21:03 +0100

    Redux.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
-import { Header, Button, Spinner, CardSection, Card } from './components/common';
-import LoginForm from './components/LoginForm';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
-class App extends Component {
-  state = { loggedIn: null };
-  
-  componentWillMount() {
-    firebase.initializeApp({
-        apiKey: 'AIzaSyAktaqx7u4-opdyUpSDZxYTAVH9S0MEDM0',
-        authDomain: 'auth-d389a.firebaseapp.com',
-        databaseURL: 'https://auth-d389a.firebaseio.com',
-        projectId: 'auth-d389a',
-        storageBucket: 'auth-d389a.appspot.com',
-        messagingSenderId: '198682215862'
-      });
-
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          this.setState({ loggedIn: true });
-        } else {
-          this.setState({ loggedIn: false });
-        }
-      });
-    }
-  
-    renderContent() {
-      switch (this.state.loggedIn) {
-        case true:
-          return (
-
-            <Card>
-            <CardSection>
-              <Button onPress={() => firebase.auth().signOut()}>
-                Log Out
-              </Button>
-              </CardSection>
-              </Card>
-          );
-        case false:
-          return <LoginForm />;
-        default:
-          return <Spinner size="large" />;
-      }
-    }
-  
-    render() {
-      return (
-        <View>
-          <Header headerText="Authentication" />
-          {this.renderContent()}
+  const App = () => {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Header headerText='Tech stack' />
+          <LibraryList />
         </View>
-      );
-    }
-  }
-  
+      </Provider>  
+    );
+  };
+
   export default App;
